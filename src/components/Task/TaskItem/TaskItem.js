f7e5f4681daebb84524b329f5f089466dab6c997
VorontsoV <voroncov12-93@mail.ru>
2022-11-10 17:07:31 +1000

    final

--- src/components/Task/TaskItem/TaskItem.js
+++ src/components/Task/TaskItem/TaskItem.js
@@ -21,8 +21,8 @@ export default class TaskItem extends React.Component {
     }
     this.onPressEnter = (e) => {
       const { task, onDeleted } = this.props
-      if (!task.content) onDeleted(task.id)
       if (e.key === 'Enter') {
+        if (!task.content) onDeleted(task.id)
         this.setState(({ editing }) => {
           return {
             editing: !editing,
@@ -40,7 +40,7 @@ export default class TaskItem extends React.Component {
   render() {
     const { onDeleted, onToggleDone, task } = this.props
     const { content, done, updated, creationDate, updatedDate } = task
-    const { onChangeEditing, onPressEnter, onToggleEditing } = this.state
+    const { onChangeEditing, onPressEnter, onToggleEditing } = this
     const { editing } = this.state
 
     return (
@@ -54,7 +54,7 @@ export default class TaskItem extends React.Component {
               {updated ? formatDistanceToNow(updatedDate) : formatDistanceToNow(creationDate)} ago
             </span>
           </label>
-          <button className="icon icon-edit" onClick={this.onToggleEditing} />
+          <button className="icon icon-edit" onClick={onToggleEditing} />
           <button className="icon icon-destroy" onClick={onDeleted} />
         </div>
         {editing ? (
